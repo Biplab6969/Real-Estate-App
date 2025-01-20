dd59e2c2ea25955cf66f5365f65b564747dca002
biplab <jbiplab80@gmail.com>
2025-01-21 00:00:10 +0530

    auth complete

---
+++ Real-Estate-Backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js";
+import Authrouter from "./routes/auth.route.js";
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(() => {
+  console.log("Connected to Mongodb");
+})
+.catch((err) =>{
+  console.log(err);
+})
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('server is running at port 3000');
+})
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', Authrouter);
