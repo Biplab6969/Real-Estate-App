b6fa3c55eb76bb5fec395e8d29c41f3925c6fee8
biplab <jbiplab80@gmail.com>
2025-01-21 22:49:13 +0530

    sign up complete

--- Real-Estate-Backend/controller/auth.controller.js
+++ Real-Estate-Backend/controller/auth.controller.js
@@ -1,7 +1,17 @@
 import User from '../models/user.model.js'
-export const signup = async (req, res) => {
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json('User created sucessfully!');
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try{
+    await newUser.save();
+    
+     res.status(201).json('User created sucessfully!');
+  } catch(error) {
+    
+    next(error);
+  }
+
 }
