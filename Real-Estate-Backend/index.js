7f3e1e108a0063a0ede1002b501bbaa48808bed6
biplab <jbiplab80@gmail.com>
2025-01-27 15:53:52 +0530

    create listing api

--- Real-Estate-Backend/index.js
+++ Real-Estate-Backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js";
 import Authrouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -23,6 +24,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', Authrouter);
+app.use('/api/listing', listingRouter)
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
